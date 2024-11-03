982db52986325cc35d82e26e587a7fc14c6c8eca
MeFerdi <ferdinandodhis254@gmail.com>
2024-11-03 15:59:28 +0300

    Chore: implement mru caching system

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,62 +1,67 @@
 #!/usr/bin/env python3
-""" MRUCache module implementing an MRU caching system
+"""Create a class MRUCache that inherits
+from BaseCaching and is a caching system:
+
+You must use self.cache_data - dictionary
+from the parent class BaseCaching
+You can overload def __init__(self): but don’t
+forget to call the parent init: super().__init__()
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the
+item value for the key key.
+If key or item is None, this method should not do anything.
+If the number of items in self.cache_data is higher
+that BaseCaching.MAX_ITEMS:
+you must discard the most recently used item
+(MRU algorithm)
+you must print DISCARD: with the key discarded and
+following by a new line
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in
+self.cache_data, return None.
 """
 
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class MRUCache(BaseCaching):
-    """ MRUCache inherits from BaseCaching and implements an MRU caching system.
+    """_summary_
     """
 
     def __init__(self):
-        """ Initialize MRUCache and call parent constructor.
+        """_summary_
         """
         super().__init__()
-        self.order = []  # To keep track of the order of keys for MRU
+        self.usedKeys = []
 
     def put(self, key, item):
-        """ Assigns an item to the cache data dictionary.
+        """_summary_
 
         Args:
-            key (str): The key for the item to be stored.
-            item (Any): The value to be stored in the cache.
-
-        If key or item is None, this method does nothing.
-        If adding this item exceeds MAX_ITEMS, discard the most recently used item.
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
         if key is not None and item is not None:
-            if key in self.cache_data:
-                # Update existing key's value
-                self.cache_data[key] = item
-                # Move this key to the end of order list to mark it as recently used
-                self.order.remove(key)
-                self.order.append(key)
+            self.cache_data[key] = item
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
             else:
-                # Add new key-value pair
-                self.cache_data[key] = item
-                self.order.append(key)  # Track order of keys
-
-            # Check if we exceed max items
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                # Discard the most recently used (last in order)
-                discarded_key = self.order.pop()  # Remove last key (MRU)
-                del self.cache_data[discarded_key]  # Remove from cache
-                print("DISCARD: {}".format(discarded_key))
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                discard = self.usedKeys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """ Retrieves an item from the cache by key.
+        """return the value in self.cache_data linked to key
 
         Args:
-            key (str): The key for the item to retrieve.
-
-        Returns:
-            The value associated with the key if it exists; None otherwise.
+                        key (_type_): _description_
         """
-        if key is None or key not in self.cache_data:
-            return None
-        
-        # Update usage by moving this key to the end of order list
-        self.order.remove(key)
-        self.order.append(key)
-
-        return self.cache_data[key]
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
+        return None
