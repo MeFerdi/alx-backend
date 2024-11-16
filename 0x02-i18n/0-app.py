149f0761a81b299b8fe7552624b966601b4dab5f
MeFerdi <ferdinandodhis254@gmail.com>
2024-11-16 11:55:51 +0300

    Chore: initializing basic setup of flask

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A basic Flask application with a single route.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home() -> str:
+    """
+    Renders the home page with a welcome message.
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
