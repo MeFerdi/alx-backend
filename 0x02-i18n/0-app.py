44279f40dce452a103839f06975f4b14cf89d797
MeFerdi <ferdinandodhis254@gmail.com>
2024-11-16 11:58:13 +0300

    Chore: initializing basic setup of flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 """
-A basic Flask application with a single route.
+A basic Flask application with Babel configuration for localization.
 """
 
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration for Flask app with Babel integration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
 
 @app.route('/')
 def home() -> str:
@@ -13,6 +27,7 @@ def home() -> str:
     Renders the home page with a welcome message.
     """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
